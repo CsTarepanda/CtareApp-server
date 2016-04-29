e842718f66ae997b169a91a1deaf2062b8b18a31
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-04-29 10:58:29 +0900

    wsserverのテンプレート完成

--- ws.py
+++ ws.py
@@ -1,56 +1,58 @@
 import tornado.ioloop
 import tornado.websocket
+from lib.mylib_cls import Json
 
-# import ws_settings
-
+debug = True
+def debug(mes):
+    if debug: print(mes)
 
 class WebSocket(tornado.websocket.WebSocketHandler):
+    # URL = r"/$"
+
     def check_origin(self, origin):
-        print(origin)
+        debug(origin)
         self.username, self.secret = WebSocket.get_account(self.request.headers)
         return True
 
     def open(self):
         self.client_type = self.request.headers.get("Type")
         self.client_type = self.client_type if self.client_type else "sender"
-        print("connect: " + self.username +" type: "+ self.client_type)
+        debug("connect: " + self.username +" type: "+ self.client_type)
         if self.client_type == "receiver":
             self.__class__.add_client(self)
-            print("websocket opened: " +self.username)
-        print("{0} : {1}".format(self.__class__, self.clients))
-        print(self.client_type)
+            debug("websocket opened: " +self.username)
+            self.__class__.send_message(self, Json(
+                username="system",
+                message="Hello, " + self.username,
+                ))
+        debug("{0} : {1}".format(self.__class__, self.clients))
+
 
     def on_close(self):
         if self.client_type == "receiver":
             self.__class__.del_client(self)
-            print("websocket closed: " +self.request.headers["User"])
+            debug("websocket closed: " +self.request.headers["User"])
         self.close()
-        print(self.clients)
+        debug(self.clients)
 
-    # @classmethod
-    # def send_message(cls, target, message):
-    #     print(cls.clients)
-    #     for client in cls.get_client(target)[0]:
-    #         print("ok : {}".format(cls))
-    #         client.write_message(str(message))
-    #
-    # @classmethod
-    # def all_send_message(cls, message):
-    #     for clie in cls.clients.values():
-    #         for c in clie:
-    #             c.write_message(str(message))
-    #
-    # def other_send_message(self, message):
-    #     me = WebSocket.get_client(self)[0]
-    #     for clie in WebSocket.clients.values():
-    #         if clie != me:
-    #             for c in clie:
-    #                 c.write_message(str(message))
-    #
-    # @staticmethod
-    # def _extract_user(ws):
-    #     return ws.request.headers["User"]
-    #
+    @classmethod
+    def send_message(cls, target, message):
+        for client in cls.clients[target.username]:
+            client.write_message(str(message))
+
+    @classmethod
+    def all_send_message(cls, message):
+        for client in cls.clients.values():
+            for ws in client:
+                ws.write_message(str(message))
+
+    def other_send_message(self, message):
+        selfclient = self.__class__.clients[self.username]
+        for client in WebSocket.clients.values():
+            if client != selfclient:
+                for ws in client:
+                    ws.write_message(str(message))
+
     @classmethod
     def add_client(cls, ws):
         if not ws.username in cls.clients:
@@ -62,36 +64,22 @@ class WebSocket(tornado.websocket.WebSocketHandler):
         cls.clients[ws.username].remove(ws)
         if not cls.clients[ws.username]:
             del cls.clients[ws.username]
-    #
-    # @classmethod
-    # def get_client(cls, ws):
-    #     username = WebSocket._extract_user(ws)
-    #     return cls.clients[username], username
-    #
+
     @staticmethod
     def get_account(data):
         return data.get("User"), data.get("Secret")
 
 
 if __name__ == "__main__":
+    import settings
     wsservers = []
-    # for i in ws_settings.SERVERS:
-    #     mod = __import__(i +".ws")
-    #     wsservers.append((mod.ws.SERVER_URL, mod.ws.WebSocket))
-    # application = tornado.web.Application(wsservers)
-    #
-    # application.listen(8000)
-    # print("runserver: " + ws_settings.URL)
-    # print("run apps:\n - {}".format("\n - ".join(ws_settings.SERVERS)))
-    # tornado.ioloop.IOLoop.instance().start()
-
-    WebSocket.clients = {}
-    wsservers.append((r"/$", WebSocket))
-
-    import test.ws as sub
-    sub.WebSocket.clients = {}
-    wsservers.append((r"/sub/$", sub.WebSocket))
+    for appname in settings.APPLICATIONS:
+        mod = __import__("{name}.ws".format(name=appname))
+        mod.ws.WebSocket.clients = {}
+        wsservers.append((mod.ws.WebSocket.URL, mod.ws.WebSocket))
+    print("runserver port: {}".format(settings.PORT))
+    print("run apps:\n - {}".format("\n - ".join(settings.APPLICATIONS)))
 
     application = tornado.web.Application(wsservers)
-    application.listen(8000)
+    application.listen(settings.PORT)
     tornado.ioloop.IOLoop.instance().start()
