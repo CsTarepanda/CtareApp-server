d9b6d63c8d37b7d6a2210109871c03e579e8bc42
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-09 12:39:26 +0900

    fix Root -> Router

--- templateServer/app.py
+++ templateServer/app.py
@@ -1,8 +1,8 @@
 import ws
 from ws import router
 
-class Root(ws.WebSocket, ws.Root):
-    BASE_URL = ws.Root.plus("sample")
+class Root(ws.WebSocket, ws.Router):
+    BASE_URL = ws.Router.plus("sample")
 
 @router("samplews")
 class Sample(Root):
