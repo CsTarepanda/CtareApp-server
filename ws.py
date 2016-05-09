2127861eea2fea1f47a0ddb3d9aa370df7e53abe
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-09 12:31:04 +0900

    urlの指定法を変更 decorator

--- ws.py
+++ ws.py
@@ -6,9 +6,17 @@ debug = True
 def debug(mes):
     if debug: print(mes)
 
-class WebSocket(tornado.websocket.WebSocketHandler):
-    # URL = r"/$"
 
+class Router:
+    BASE_URL = ""
+
+    @classmethod
+    def plus(cls, url):
+        url = url if url.startswith("/") else "/{}".format(url)
+        return cls.BASE_URL + url
+
+
+class WebSocket(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         debug(origin)
         self.username, self.secret = WebSocket.get_account(self.request.headers)
@@ -70,17 +78,39 @@ class WebSocket(tornado.websocket.WebSocketHandler):
         return data.get("User"), data.get("Secret")
 
 
+class Application:
+    servers = []
+    INSTANCE = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.INSTANCE is None:
+            cls.INSTANCE = object.__new__(cls)
+        return cls.INSTANCE
+
+    def __init__(self, url, server):
+        self.servers.append((url, server))
+
+
+def route(url_string="/"):
+    print(Application)
+    url_string = url_string if url_string.startswith("/") else "/{}".format(url_string)
+    def decorator(cls):
+        cls.clients = {}
+        Application(cls.BASE_URL + url_string, cls)
+        return cls
+    return decorator
+
 if __name__ == "__main__":
     import settings
     from importlib import import_module
-    wsservers = []
+    servers = []
     for appname in settings.APPLICATIONS:
-        mod = import_module("{name}.ws".format(name=appname))
-        mod.WebSocket.clients = {}
-        wsservers.append((mod.WebSocket.URL, mod.WebSocket))
+        mod = import_module("{name}.app".format(name=appname))
+        servers += mod.ws.Application.servers
     print("runserver port: {}".format(settings.PORT))
     print("run apps:\n - {}".format("\n - ".join(settings.APPLICATIONS)))
 
-    application = tornado.web.Application(wsservers)
+    print("urls:", servers)
+    application = tornado.web.Application(servers)
     application.listen(settings.PORT)
     tornado.ioloop.IOLoop.instance().start()
